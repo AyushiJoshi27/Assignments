47585fa46e1abee19698cd454c7f2da331d33060
AyushiJoshi27 <ayushi.joshi@newput.com>
2023-01-01 19:04:30 +0530

    new-year

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 $('.openbtn').click(function(){
-  document.getElementById("mySidebar").style.width = "25%";
-  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
 });
 $('.closebtn').click(function(){
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
 });
 var path = {
-  'main' : 'https://dog.ceo/api/breeds/image/random', 'trio' : 'https://dog.ceo/api/breeds/image/random/3',
-  'by' : 'https://dog.ceo/api/breed/hound/images/random', 'tri' : 'https://dog.ceo/api/breed/hound/images/random/3',
-  'sub' : 'https://dog.ceo/api/breed/hound/afghan/images/random', 'thri' : 'https://dog.ceo/api/breed/hound/afghan/images/random/3'
+  'main' : 'https://dog.ceo/api/breeds/image/random', 'trio' : 'https://dog.ceo/api/breeds/image/random/3', 'by' : 'https://dog.ceo/api/breed/hound/images/random',
+  'tri' : 'https://dog.ceo/api/breed/hound/images/random/3', 'sub' : 'https://dog.ceo/api/breed/hound/afghan/images/random', 'thri' : 'https://dog.ceo/api/breed/hound/afghan/images/random/3'
 };
 
 $('.doc-block').hide();
@@ -24,25 +23,30 @@ $('.btn-block').click(function() {
   var handle = $(this).attr('data');
   $.ajax({
     type : 'GET',
-    dataType : 'JSON',
     url : path[handle],
     product : {},
     success : function(product) {
       $('.img-' + handle).hide();
-      let len = product.message;
-      console.log(len);
+      //let len = product.message;
+      //console.log(len);
+      if (product.message.length <= 3) {
+        $('.table-block-' + handle).empty();
+        for (i = 0; i < product.message.length; i++) {
+          $('.table-block-' + handle).append(`<tr><td><img src='${product.message[i]}' class='img-api'><td><tr>`);
+        };
+      } else {
+        $('.table-' + handle).html(`<tr><td><img src='${product.message}' class='img-api'><td><tr>`);
+      }
     }
   });
 });
 $.ajax({
   type: 'GET',
   url: 'https://dog.ceo/api/breed/hound/list',
-  dataType: 'JSON',
   list: {},
   success: function (list) {
-    var names = $('#list');
     for (var i = 0; i < 7; i++) {
-      names.append(`<p>${list.message[i]}</p><br>`);
+      $('#list').append(`<p style="text-transform:capitalize;">${list.message[i]}</p><br>`);
     };
   }
 });
